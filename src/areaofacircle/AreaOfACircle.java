d5e4c4aae1b37c1886ad990d7ac2cd6c7eabbc3f
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-09-18 09:09:00 -0400

    This is the area of a circle

---
+++ src/areaofacircle/AreaOfACircle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package areaofacircle;
+
+/**
+ *
+ * @author pehor9164
+ */
+public class AreaOfACircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double radius = 15;
+        double pie = 3.141582;
+        System.out.println("The Radius Of The Circle Is " + pie * radius * radius );
+    }
+    
+}
